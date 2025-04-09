db133f7014ede302e7f26aa4f96949ffeaa2e01f
senella02 <thananan.pl@gmail.com>
2025-04-09 18:38:03 +0700

    Added database connecting and schema

---
+++ src/lib/db.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+import { MONGO_URL } from '$env/static/private';
+
+const mongoConnection = {
+  isConnecting : 0,
+}
+
+const connectDB = async() => {
+  if(mongoConnection.isConnecting === 1) {
+    console.log('Database is successfully connected');
+    return;
+  }
+
+  if(mongoose.connections.length > 0) {
+    mongoConnection.isConnecting = mongoose.connections[0].readyState;
+    if(mongoConnection.isConnecting === 1){
+      console.log('Database is successfully connected');
+      return;
+    }
+    else{
+      await mongoose.disconnect();
+    }
+  }
+
+  await mongoose.connect(MONGO_URL || '', {
+    dbName : 'jaedeesai',});
+    mongoConnection.isConnecting = 1;
+    console.log('New connection to mongodb');
+}
+
+const disconnectDB = async() => {
+  if(mongoConnection.isConnecting === 0) return;
+  await mongoose.disconnect();
+  mongoConnection.isConnecting = 0;
+  console.log('Disconnected to mongoDB');
+}
+
+export {connectDB, disconnectDB};
