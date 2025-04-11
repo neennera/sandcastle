e3c569263099959ce78ba9711e37b066f10bbb0e
Nattarin Chetpattananondh <nattarinchet@gmail.com>
2025-04-11 21:29:55 +0700

    add btn

--- src/lib/components/Decopanel.svelte
+++ src/lib/components/Decopanel.svelte
@@ -17,42 +17,47 @@
     >
       ตกแต่งเพิ่ม
     </button>
+
+    <div class="flex flex-col w-full h-full">
     {#if isOpen}
-    <div class="w-[90%] h-[90%] flex items-center justify-center mt-[4%] ml-[5%]">
-      <div class="grid grid-cols-2 grid-rows-3 w-[80%] h-full ">
-        {#each decoprops as deco, index (index)}
-        <div
-            class="w-24 h-24 flex items-center justify-center rounded-lg hover:scale-120 transition-transform duration-200 ease-in-out"
-            on:click={() => (clickedIndex = index)}
-            on:keydown={(event) => {
-                if (event.key === 'Enter' || event.key === ' ') {
-                    event.preventDefault();
-                    clickedIndex = index;
-                }
-            }}
-            role="button"
-            tabindex="0"
-            aria-label="Select decoration"
-        >
-            <img src={clickedIndex == index ? deco.focusedimg : deco.imgsrc} alt="Decoration" class="w-full h-full object-cover rounded-lg" />
+     <div class="w-[90%] h-[70%] flex items-center justify-center mt-[4%] ml-[5%]">
+        <div class="grid grid-cols-2 grid-rows-3 w-[80%] h-full ">
+            {#each decoprops as deco, index (index)}
+            <div
+                class="w-24 h-24 flex items-center justify-center rounded-lg hover:scale-120 transition-transform duration-200 ease-in-out"
+                on:click={() => (clickedIndex = index)}
+                on:keydown={(event) => {
+                    if (event.key === 'Enter' || event.key === ' ') {
+                        event.preventDefault();
+                        clickedIndex = index;
+                    }
+                }}
+                role="button"
+                tabindex="0"
+                aria-label="Select decoration"
+            >
+                <img src={clickedIndex == index ? deco.focusedimg : deco.imgsrc} alt="Decoration" class="w-full h-full object-cover rounded-lg" />
+            </div>
+            {/each}
         </div>
-        {/each}
-      </div>
-      <div class="w-full h-full text-[#bd7878] justify-center items-center text-xl font-semibold flex flex-col ml-4 pl-4">
-        {#if clickedIndex !== null}
-        <h1 class="font-bold text-2xl">{decoprops[clickedIndex].name}</h1>
-        <h2>{decoprops[clickedIndex].detail}</h2>
-        <h2 class="mt-4 ml-1 self-start text-lg">คำอธิบาย:</h2>
-        <h1 class="text-base font-medium self-start mt-1 ml-1">{decoprops[clickedIndex].description}</h1>
-        {:else}
-        จิ้มของที่ชอบเลย!
-        {/if}
+        <div class="w-full h-full text-[#bd7878] justify-center items-center text-xl font-semibold flex flex-col ml-4 pl-4">
+            {#if clickedIndex !== null}
+            <h1 class="font-bold text-2xl">{decoprops[clickedIndex].name}</h1>
+            <h2>{decoprops[clickedIndex].detail}</h2>
+            <h2 class="mt-4 ml-1 self-start text-lg">คำอธิบาย:</h2>
+            <h1 class="text-base font-medium self-start mt-1 ml-1">{decoprops[clickedIndex].description}</h1>
+            {:else}
+            จิ้มของที่ชอบเลย!
+            {/if}
         </div>
-    </div>
+     </div>
+    <button class="btn-secondary self-center mt-4 mb-4">ส่งของตกแต่ง</button>
     {/if}
+    </div>
 </div>
+    
   
-  <div
+<div
     class="bg-[#f6f5ea] opacity-0 w-full h-full absolute rounded-[20px] z-0"
     id="blur"
     on:click={(event) => { handleClickOutside(event); clickedIndex = null; isOpen=false;}}
@@ -63,5 +68,5 @@
     }}
     role="button"
     tabindex="0"
-  ></div>
+></div>
   
