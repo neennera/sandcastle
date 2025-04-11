3d096e0fd63c9234e44830715738751558289b13
senella02 <thananan.pl@gmail.com>
2025-04-12 02:33:38 +0700

    Change API provider

--- src/routes/api/auth/get-otp/+server.js
+++ src/routes/api/auth/get-otp/+server.js
@@ -2,35 +2,36 @@ import { connectDB } from '$lib/db.js';
 import OTP from '$lib/models/otp';
 import { json } from '@sveltejs/kit';
 import { randomInt } from 'crypto';
-import { SENDGRID_API_KEY, EMAIL_USER } from '$env/static/private';
-import sgMail from '@sendgrid/mail';
+import { MAILGUN_API_KEY, MAILGUN_DOMAIN, EMAIL_USER } from '$env/static/private';
+import FormData from 'form-data';
+import Mailgun from 'mailgun.js';
 import jaedeesai from '$lib/models/jaedeesai.js';
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+const mailgun = new Mailgun(FormData);
+const mg = mailgun.client({ username: 'api', key: MAILGUN_API_KEY });
+
 export async function POST({ request, locals }) {
     if (locals.user === null) {
         return json({ message: "Unauthorized user" }, { status: 401 });
     }
 
-    const { email } = await request.json();
-    if (!email) {
+    const { email, ownername } = await request.json();
+    if (!email || !ownername) {
         return json({ message: "Missing required field" }, { status: 400 });
     }
 
     await connectDB();
     const existingOtp = await OTP.findOne({ email }).select('_id').lean();
     if (existingOtp) {
-        return json({ message: "We already sent an email, please check your junk email" }, { status: 400 });
+        return json({ message: "We already sent an email, please check your junk mail" }, { status: 400 });
     }
     const existingUser = await jaedeesai.findOne({ email }).select('_id').lean();
     if (existingUser) {
         return json({ message: "This email has already used" }, { status: 400 });
     }
 
-
     let otp;
     try {
-
         while (true) {
             otp = randomInt(100000, 1000000);
             try {
@@ -39,30 +40,51 @@ export async function POST({ request, locals }) {
                     otp,
                 }).save();
                 break;
-            }
-            catch (err) {
+            } catch (err) {
                 if (err instanceof Error && typeof err === 'object' && 'code' in err) {
                     if (err.code !== 11000) {
                         console.error(err.message);
                         return json({ message: 'Error trying to save OTP' }, { status: 500 });
                     }
-                }
-                else {
+                } else {
                     if (err instanceof Error) console.error(err.message);
                     return json({ message: 'Unknown error trying to create OTP' }, { status: 500 });
                 }
             }
         }
 
+        const emailContent = `
+            <html>
+            <body>
+                <h1>Confirm Your E-mail verification on Jaedeesai</h1>
+                <p>สวัสดี <strong>${ownername}!😊</strong>,</p>
+                <p>ขอบคุณมาร่วมสร้างเจดีย์ทราย บนเว็บไซต์ Jaedeesai ของพวกเรา✨</p>
+                <p>กรุณานำ<strong> รหัส OTP ด้านล่าง</strong>ไประบุในหน้ายืนยันอีเมลเพื่อเข้าใช้งาน</p>
+                <div style="font-size: 24px; font-weight: bold; padding: 10px; background-color: #ffecb3; text-align: center;">
+                <strong>${otp}</strong>
+                </div>
+                <p>โปรดดำเนินการยืนยันอีเมล<strong>ภายใน 10 นาที</strong></p>
+                <p>หากมีข้อสงสัยเพิ่มเติมกรุณาติดต่อ...</p>
+            </body>
+            </html>
+        `;
+
         const msg = {
-            to: email,
-            from: EMAIL_USER,
-            subject: "Your OTP code",
-            text: `Your OTP code is ${otp}`
+            from: `Mailgun Sandbox <postmaster@${MAILGUN_DOMAIN}>`,
+            to: [`${ownername} <${email}>`],
+            subject: "OTP for E-mail Address verification on Jaedeesai",
+            html: emailContent
         };
 
-        await sgMail.send(msg);
-        return json({ message: 'Successfully created OTP and sent email!' }, { status: 200 });
+        try {
+            await mg.messages.create(MAILGUN_DOMAIN, msg);
+            return json({ message: 'Successfully created OTP and sent email!' }, { status: 200 });
+        } catch (emailError) {
+            console.error('Failed to send email:', emailError.message);
+
+            await OTP.deleteOne({ email, otp });
+            return json({ message: 'Failed to send email. OTP has been removed.' }, { status: 500 });
+        }
 
     } catch (err) {
         if (err instanceof Error) {
