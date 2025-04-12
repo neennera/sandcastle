9eba827508abb00d323a653349f8711787a30e2b
Flook0147 <flook.akkharaphon@gmail.com>
2025-04-12 15:39:48 +0700

    edit

--- src/lib/components/CreditPopup.svelte
+++ src/lib/components/CreditPopup.svelte
@@ -9,7 +9,7 @@
 	<div class="flex h-[30%] w-full items-center justify-center">
 		<h3 class="text-4xl">credit</h3>
 	</div>
-	<div class="items-left flex h-full w-full flex-col justify-start pt-4">
+	<div class="items-left flex h-full w-full flex-col justify-start px-4 pt-4">
 		<div class="flex flex-col">
 			<h3 class="mb-3 flex items-center text-xl">
 				source code
@@ -19,10 +19,10 @@
 				>
 			</h3>
 		</div>
-		<h3 class="mb-3 text-xl">ทีมพัฒนา: neenneraม, Flook0147,</h3>
-		<h3 class="mb-3 text-xl">Cchinchillaz, senella02</h3>
-		<h3 class="mb-3 text-xl">ทีมกราฟฟิก: Pakpim</h3>
-		<h3 class="mb-3 text-xl">feedback: xxxxxxxxxx</h3>
+		<h3 class="mb-3 text-lg">ทีมพัฒนา: neennera, Flook0147,</h3>
+		<h3 class="mb-3 text-lg">Cchinchillaz, senella02</h3>
+		<h3 class="mb-3 text-lg">ทีมกราฟฟิก: Pakpim</h3>
+		<h3 class="mb-3 text-lg">feedback: xxxxxxxxxx</h3>
 	</div>
 	<button
 		class="absolute top-4 right-3 flex h-8 w-8 items-center justify-center rounded-full bg-[#d9d9d9] text-[24px] font-bold text-[#8d7878] opacity-80 hover:opacity-100 sm:h-10 sm:w-10 sm:text-[32px]"
